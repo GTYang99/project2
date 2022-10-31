be938edf92d8d5e5d95bf26f26f6de88d60a6487
GT.Yang <gaga0099@gmail.com>
2022-10-31 15:13:50 +0800

    按鈕修改為能中斷

--- main1.py
+++ main1.py
@@ -1,9 +1,15 @@
 from gpiozero import Button
+from signal import pause
+
+def say_hello():
+    print('Hello!')
+
+def say_goodbye():
+    print('Goodbye!')
 
 button = Button(18)   #第18針腳建立一個按鈕
 
-while True:
-    if button.is_pressed:
-        print("Button is pressed")
-    else:
-        print("Button is not pressed")
+button.when_pressed = say_hello
+button.when_released = say_goodbye
+
+pause()
