82f6cb867772442ad323bf8c2b5fc4c07df88821
GT.Yang <gaga0099@gmail.com>
2022-11-01 11:37:30 +0800

    按下按鈕回傳顏色文字

--- main2.py
+++ main2.py
@@ -38,7 +38,7 @@ class ColorCanvas(tk.Canvas):
             #多加小圓點
             print("多加小圓點")
             rec_width = self.width - 2 * self.space
-            rec_height = self.height - 2 * self.space
+            # rec_height = self.height - 2 * self.space
             cir_width = rec_width / 5
             cir_height = rec_height / 5
             cir_start_x = self.space + cir_width / 2
@@ -50,40 +50,26 @@ class ColorCanvas(tk.Canvas):
 class Windows(tk.Tk):
     def __init__(self):
         super().__init__()
-        # red = ColorCanvas(self,width=70,height=70,relief='ridge')
-        # red.create_rectangle(10,10,60,60,fill="red")
-        # 原點所在的座標位置
-        # x  = 70*1/5
-        # y  = 70*4/6
-        # width = 10
-        # red.create_oval(x,y,x+10,y+10,fill="white",outline='white')
-        # red = ColorCanvas(self,"red",width=70,height=70)
         red = ColorCanvas(self,"red",width=100,height=100)
         red.bind('<ButtonRelease-1>',self.mouse_click)
         red.grid(row=0, column=0)
-        red.state = ColorCanvas.ON
         print(f'red狀態:{red.state}')
 
-        # green = tk.Canvas(self,width=70,height=70)
-        # green.create_rectangle(10,10,60,60,fill="green")
         green = ColorCanvas(self,"green",width=100,height=100)       
-        green.grid(row=0, column=1)
-        green.state = ColorCanvas.ON
+        green.bind('<ButtonRelease-1>',self.mouse_click)        
+        green.grid(row=0, column=1)        
 
-        # blue = tk.Canvas(self,width=70,height=70)
-        # blue.create_rectangle(10,10,60,60,fill="blue")
         blue = ColorCanvas(self,"blue",width=100,height=100)   
+        blue.bind('<ButtonRelease-1>',self.mouse_click)
         blue.grid(row=0, column=2)
-        blue.state = ColorCanvas.ON
+
 
     def mouse_click(self,event):
         print(event.__dict__)
-        # 圖斑做出邊框
-        # event.widget.config(borderwidth=5)
-        # event.widget.delete()
-        event.widget.create_rectangle(10,10,60,60,fill='white')
-        event.widget.create_rectangle(20,20,50,50,fill='red')
-        # event.widget.update()
+        print(event.widget.rec_color)
+        # event.widget.create_rectangle(10,10,60,60,fill='white')
+        # event.widget.create_rectangle(20,20,50,50,fill='red')
+
 
 
 def main():
